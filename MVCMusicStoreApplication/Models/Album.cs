f3eccb46c2850d71e309ab8d5102ecbfd13e1e40
russellcriblez27 <s00931148@acad.tri-c.edu>
2018-09-29 14:11:36 -0400

    Lab 05. Video follow along.

--- MVCMusicStoreApplication/Models/Album.cs
+++ MVCMusicStoreApplication/Models/Album.cs
@@ -16,5 +16,7 @@ namespace MVCMusicStoreApplication.Models
         public virtual Artist Artist { get; set; }
         public virtual string AlbumArtUrl {get;set;}
         public virtual string AdditionalInfo { get; set; }
+        public virtual string CountryOfOrigin { get; set; }
+        public virtual bool InStock { get; set; }
     }
 }
