ca930be8c0558f109f1ce351d140d8a9e7135685
russellcriblez27 <s00931148@acad.tri-c.edu>
2018-11-03 11:49:48 -0400

    Lab 10. Store front added. browse index and details added.

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            var Albums = db.Albums.Where(a => a.GenreId == id);
+            return View(Albums);
+        }
+
+        //GET: Browse
+        public ActionResult Browse()
+        {
+            var Genres = db.Genres.ToList();
+            return View(Genres);
+        }
+
+        //GET: Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albums.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+    }
+}
